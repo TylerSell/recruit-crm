fc2ef90fcb066a6e30101ceadf08b16250fc8c9e
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-04-29 14:24:06 -0500

    start building user/recruiter_controller

--- app/controllers/user/recruiters_controller.rb
+++ app/controllers/user/recruiters_controller.rb
@@ -1,2 +1,45 @@
 class User::RecruitersController < ApplicationController
+    before_action :find_recruiter, only: [:index, :show, :edit, :update]
+    
+    def index
+        @candidates = @recruiter.candidates
+    end
+    
+    def new
+        @recruiter = Recruiter.new
+    end 
+
+    def create
+        @recruiter = Recruiter.create(recruiter_params)
+
+        if @recruiter.save 
+            session[:user_id] = @recruiter.id 
+            redirect_to user_recruiters_path(@recruiter)
+        else 
+            render 'recruiter/new'
+        end
+
+    end 
+
+    def show
+    end 
+
+    def edit 
+    end 
+
+    def update
+    end 
+
+# --------------------------PRIVATE METHODS---------------------------------------
+
+    private 
+
+    def recruiter_params 
+        pararms.require(:recruiter).permit(:first_name, :last_name, :phone_number, :email, :password)
+    end 
+
+    def find_recruiter
+        @recruiter = Recruiter.find_by(id: params[:id])
+    end
+
 end
