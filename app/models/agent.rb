c4d13a10a2c7a7b8970318c1761a7d2279d156c3
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-04-29 11:50:07 -0500

    add Agent and Recruiter model under User and build their associations

---
+++ app/models/agent.rb
@@ -0,0 +1,5 @@
+class Agent < User
+    has_many :candidates, :foreign_key => :agent_id 
+    has_many :recruiters, :through => :candidates
+    # Agent.new would save to the User table with a type of 'Agent'
+end
---
+++ app/models/recruiter.rb
@@ -0,0 +1,5 @@
+class Recruiter < User
+    has_many :candidates, :foreign_key => :recruiter_id 
+    has_many :agents, :through => :candidates
+    # Recruiter.new would save to the User table with a type of 'Recruiter'
+end
