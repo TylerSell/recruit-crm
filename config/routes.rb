f181c1effaaf18c7e5c9951b6c4ebc2316a9cb26
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-01 11:54:58 -0500

    building routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
 
   root 'static#home'
 
-  
+  get '/login', to: 'user/sessions#new'
+  post '/login', to: 'user/sessions#create'
+
+  get '/logout', to: 'user/sessions#destroy'
+
+  namespace :user do 
+    resources :agents, :recruiters, only: [:index, :new, :create, :edit, :update]
+  end
 
   resources :candidates
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
