68fa4cb922fd0775effd4e4700d7d1a371d54de2
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-01 11:07:40 -0500

    build application_controller and session_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :redirect_if_not_authenticated 
+    helper_methods :current_user, :agent, :user_authenticated
+
+    # ----------------------PRIVATE METHODS BELOW----------------------------------
+
+    private 
+
+    def agent 
+        current_user.type == "Agent" ? true : false
+    end 
+
+    def current_user
+        @user ||= User.find_by(id: session[:user_id])
+    end 
+
+    def user_authenticated
+        !!session[:user_id]
+    end 
+
+    def redirect_if_not_authenticated
+        redirect_to root_path unless user_authenticated
+    end
+
 end
--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,2 +1,27 @@
 class User::SessionsController < ApplicationController
+
+    def new
+        @user = User.new 
+    end 
+
+    def create 
+        @user = User.find_by(email: params[:email])
+
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id 
+            if agent 
+                redirect_to user_agents_path(@user)
+            else 
+                redirect_to user_recruiters_path(@user)
+            end 
+        else 
+            redirect_to root_path
+        end 
+    end 
+
+    def destroy 
+        session.delete("user_id")
+        redirect_to root_path
+    end 
+    
 end
