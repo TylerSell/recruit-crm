3108d2610b23d421e2e1f57500994e43f81c9f84
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-08 16:01:01 -0500

    fix logic error in static controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,12 +2,14 @@ class StaticController < ApplicationController
     skip_before_action :redirect_if_not_authenticated, only: [:home]
 
     def home 
-        if is_agent?
-            redirect_to agent_path(current_user.id)
-        elsif user_authenticated
-            redirect_to recruiter_path(current_user.id)
+        if current_user 
+            if is_agent? 
+                redirect_to agent_path(current_user.id)
+            else 
+                redirect_to recruiter_path(current_user.id)
+            end
         else 
-            render :home 
+            render :home
         end
     end 
     
