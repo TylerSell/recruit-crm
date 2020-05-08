c841618210327911be6c485e0f151182ad138209
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-08 09:54:11 -0500

    building view help methods for states and stages

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -1,2 +1,74 @@
 module CandidatesHelper
+
+    def us_states
+        [
+            ['Alabama', 'AL'],
+            ['Alaska', 'AK'],
+            ['Arizona', 'AZ'],
+            ['Arkansas', 'AR'],
+            ['California', 'CA'],
+            ['Colorado', 'CO'],
+            ['Connecticut', 'CT'],
+            ['Delaware', 'DE'],
+            ['District of Columbia', 'DC'],
+            ['Florida', 'FL'],
+            ['Georgia', 'GA'],
+            ['Hawaii', 'HI'],
+            ['Idaho', 'ID'],
+            ['Illinois', 'IL'],
+            ['Indiana', 'IN'],
+            ['Iowa', 'IA'],
+            ['Kansas', 'KS'],
+            ['Kentucky', 'KY'],
+            ['Louisiana', 'LA'],
+            ['Maine', 'ME'],
+            ['Maryland', 'MD'],
+            ['Massachusetts', 'MA'],
+            ['Michigan', 'MI'],
+            ['Minnesota', 'MN'],
+            ['Mississippi', 'MS'],
+            ['Missouri', 'MO'],
+            ['Montana', 'MT'],
+            ['Nebraska', 'NE'],
+            ['Nevada', 'NV'],
+            ['New Hampshire', 'NH'],
+            ['New Jersey', 'NJ'],
+            ['New Mexico', 'NM'],
+            ['New York', 'NY'],
+            ['North Carolina', 'NC'],
+            ['North Dakota', 'ND'],
+            ['Ohio', 'OH'],
+            ['Oklahoma', 'OK'],
+            ['Oregon', 'OR'],
+            ['Pennsylvania', 'PA'],
+            ['Rhode Island', 'RI'],
+            ['South Carolina', 'SC'],
+            ['South Dakota', 'SD'],
+            ['Tennessee', 'TN'],
+            ['Texas', 'TX'],
+            ['Utah', 'UT'],
+            ['Vermont', 'VT'],
+            ['Virginia', 'VA'],
+            ['Washington', 'WA'],
+            ['West Virginia', 'WV'],
+            ['Wisconsin', 'WI'],
+            ['Wyoming', 'WY']
+        ]
+    end
+
+    def stages
+        [
+            ['Stage 1 - Interested', 1], 
+            ['Stage 2 - Interview Set', 2], 
+            ['Stage 3 - Affidavit Sent', 3], 
+            ['Stage 4 - Voucher Sent', 4], 
+            ['Stage 5 - Studying', 5], 
+            ['Stage 6 - Test Scheduled', 6], 
+            ['Stage 7 - Sent To Contracting', 7], 
+            ['Stage 8 - Hired', 8], 
+            ['Stage 9 - Archived/Not Interested', 9], 
+            ['Stage 10 - Archived/No Contact', 10]
+        ]
+    end
+
 end
