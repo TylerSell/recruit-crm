ee28319f30e9651a9b179d4a03287d43b59225d6
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-09 14:41:38 -0500

    add validations to agent and recruiter

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -2,9 +2,11 @@ class Agent < User
     has_many :candidates, :foreign_key => :agent_id 
     has_many :recruiters, :through => :candidates
     # Agent.new would save to the User table with a type of 'Agent'
+    
     validates :first_name, :presence => true, :length => { :minimum => 2 }
     validates :last_name, :presence => true, :length => { :minimum => 2 }
-    validates :phone_number, :presence => true, :format => { :with => /\A\d{3}-\d{3}-\d{4}\z/, :message => "xxx-xxx-xxxx format only" }, :uniqueness => true 
+    validates :phone_number, :presence => true, :uniqueness => true,  :format => { :with => /\A\d{3}-\d{3}-\d{4}\z/, :message => "xxx-xxx-xxxx format only" }
     validates :email, :uniqueness => { :case_sensitive => false }, :format => { :with => URI::MailTo::EMAIL_REGEXP }
+    validates :password, :length => { :within => 6..40, :message => "Password must be between 6 and 40 characters" }
 
 end
--- app/models/recruiter.rb
+++ app/models/recruiter.rb
@@ -2,4 +2,11 @@ class Recruiter < User
     has_many :candidates, :foreign_key => :recruiter_id 
     has_many :agents, :through => :candidates
     # Recruiter.new would save to the User table with a type of 'Recruiter'
+
+    validates :first_name, :presence => true, :length => { :minimum => 2 }
+    validates :last_name, :presence => true, :length => { :minimum => 2 }
+    validates :phone_number, :presence => true, :uniqueness => true,  :format => { :with => /\A\d{3}-\d{3}-\d{4}\z/, :message => "xxx-xxx-xxxx format only" }
+    validates :email, :uniqueness => { :case_sensitive => false }, :format => { :with => URI::MailTo::EMAIL_REGEXP }
+    validates :password, :length => { :within => 6..40, :message => "Password must be between 6 and 40 characters" }
+
 end
