3959bb3ccd1d99618e582091b46c36e9021c766e
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-09 14:36:19 -0500

    add some dates for dashboard in certain stages

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -2,4 +2,9 @@ class Agent < User
     has_many :candidates, :foreign_key => :agent_id 
     has_many :recruiters, :through => :candidates
     # Agent.new would save to the User table with a type of 'Agent'
+    validates :first_name, :presence => true, :length => { :minimum => 2 }
+    validates :last_name, :presence => true, :length => { :minimum => 2 }
+    validates :phone_number, :presence => true, :format => { :with => /\A\d{3}-\d{3}-\d{4}\z/, :message => "xxx-xxx-xxxx format only" }, :uniqueness => true 
+    validates :email, :uniqueness => { :case_sensitive => false }, :format => { :with => URI::MailTo::EMAIL_REGEXP }
+
 end
