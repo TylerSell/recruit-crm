8b4657ddb539873c67039ce154877d5bf981a191
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-12 15:53:27 -0500

    add lines commented out to test https

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module RecruitCrm
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # config.use_ssl = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
