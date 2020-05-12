f8b83453d48abc652cda5ae3a2555a3bdbc51ce7
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-11 22:06:57 -0500

    add allow blank in phone number fields in prep for adding omniauth login

--- app/models/recruiter.rb
+++ app/models/recruiter.rb
@@ -5,7 +5,7 @@ class Recruiter < User
 
     validates :first_name, :presence => true, :length => { :minimum => 2 }
     validates :last_name, :presence => true, :length => { :minimum => 2 }
-    validates :phone_number, :presence => true, :uniqueness => true,  :format => { :with => /\A\d{3}-\d{3}-\d{4}\z/, :message => "xxx-xxx-xxxx format only" }
+    validates :phone_number, :presence => true, :uniqueness => true,  :format => { :with => /\A\d{3}-\d{3}-\d{4}\z/, :message => "xxx-xxx-xxxx format only" }, :allow_blank => true
     validates :email, :uniqueness => { :case_sensitive => false }, :format => { :with => URI::MailTo::EMAIL_REGEXP }
     validates :password, :length => { :within => 6..40, :message => "Password must be between 6 and 40 characters" }
 
