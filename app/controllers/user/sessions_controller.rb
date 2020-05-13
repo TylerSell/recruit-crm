99d903aff0d97f68f10bdcea857d1ff8573baed3
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-13 11:39:22 -0500

    add find or create logic for facebook login

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -26,7 +26,26 @@ class User::SessionsController < ApplicationController
     end 
 
     def facebook
-        binding.pry
+        @user = User.find_by(email: auth.info.email)
+
+        if @user 
+            session[:user_id] = @user.id 
+            redirect_to candidates_path
+        else 
+            facebook_user = state.new 
+            facebook_user.first_name = auth.info.first_name
+            facebook_user.last_name = auth.info.last_name
+            facebook_user.phone_number = ""
+            facebook_user.email = auth.info.email
+            facebook_user.password = SecureRandom.hex(12)
+            facebook_user.save
+            if facebook_user.save
+                session[:user_id] = facebook_user.id 
+                redirect_to candidates_path
+            else
+                redirect_to root_path
+            end
+        end 
     end
 
     private 
@@ -36,7 +55,11 @@ class User::SessionsController < ApplicationController
     end
 
     def state
-        request.env['omniauth.state']
+        request.env['omniauth.params']['state']
+    end
+
+    def create_params
+
     end
     
 end
