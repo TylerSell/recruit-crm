cd8d90005feef54962a0200dec244fe1af35255a
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-13 23:23:15 -0500

    add stage views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,17 @@ Rails.application.routes.draw do
 
   get '/logout', to: 'user/sessions#destroy'
 
+  get '/stage_1', to: 'stages#stage_1'
+  get '/stage_2', to: 'stages#stage_2'
+  get '/stage_3', to: 'stages#stage_3'
+  get '/stage_4', to: 'stages#stage_4'
+  get '/stage_5', to: 'stages#stage_5'
+  get '/stage_6', to: 'stages#stage_6'
+  get '/stage_7', to: 'stages#stage_7'
+  get '/stage_8', to: 'stages#stage_8'
+  get '/stage_9', to: 'stages#stage_9'
+  get '/stage_10', to: 'stages#stage_10'
+
   # namespace :user do 
   #   resources :agents, :recruiters, only: [:index, :new, :create, :show, :edit, :update]
   # end
