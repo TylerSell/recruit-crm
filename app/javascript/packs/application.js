d405ff1a3e66f0d7a5d247a747dfecb415e329a3
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-05-20 16:18:52 -0500

    mod application.js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,10 +6,10 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
-require("jquery3")
-require("popper")
-require("bootstrap")
+require("channels").start()
+require("jquery3").start()
+require("popper").start()
+require("bootstrap").start()
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
